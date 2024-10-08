f75fadda580811d34a45d1a3351bae788c8183af
rkusan00 <renato.kusanovic@gmail.com>
2024-10-08 11:30:01 +0200

    Remove unused toolbars

--- packages/edit/src/index.ts
+++ packages/edit/src/index.ts
@@ -2,15 +2,10 @@ import baseManifest from '@tailor-cms/ce-fill-blank-manifest';
 import type { ElementManifest } from '@tailor-cms/ce-fill-blank-manifest';
 
 import Edit from './components/Edit.vue';
-import SideToolbar from './components/SideToolbar.vue';
-import TopToolbar from './components/TopToolbar.vue';
-
 const manifest: ElementManifest = {
   ...baseManifest,
   Edit,
-  SideToolbar,
-  TopToolbar,
 };
 
 export default manifest;
-export { Edit, SideToolbar, TopToolbar };
+export { Edit };
