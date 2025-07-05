9c8f9eaa5d3efa8fcdd2814c857252323c6a5222
rkusan00 <renato.kusanovic@gmail.com>
2025-07-05 18:50:59 +0200

    Add ai config

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,6 @@
+import { ai, initState, type } from '@tailor-cms/ce-fill-blank-manifest';
 import { every, omit } from 'lodash-es';
 import type { HookServices, ServerRuntime } from '@tailor-cms/cek-common';
-import { initState, type } from '@tailor-cms/ce-fill-blank-manifest';
 import type { Element } from '@tailor-cms/ce-fill-blank-manifest';
 
 // Detect if hooks are running in CEK (used for mocking end-system runtime)
@@ -84,6 +84,7 @@ export default {
   afterRetrieve,
   onUserInteraction,
   beforeDisplay,
+  ai,
 };
 
-export { type, initState };
+export { type, initState, ai };
